7f948b380994670961e66e6fde1670a5e23d4247
Joseph M <jmedina1985@hotmail.com>
2023-02-09 21:38:26 -0800

    fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,13 @@ function App() {
     // - H "Authorization: Bearer $OPENAI_API_KEY" \
 
     const APIBody = {
-      "model": "text-davinci=003"
+      "model": "text-davinci-003",
+      "prompt": "Classify the sentiment in these tweets:\n\n1. \"I can't stand homework\"\n2. \"This sucks. I'm bored 😠\"\n3. \"I can't wait for Halloween!!!\"\n4. \"My cat is adorable ❤️❤️\"\n5. \"I hate chocolate\"\n\nTweet sentiment ratings:",
+      "temperature": 0,
+      "max_tokens": 60,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
     }
 
     await fetch("https://api.openai.com/v1/completions"), {
